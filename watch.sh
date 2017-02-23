c9751f41641618aa23ead323ab377c75df2711b8
Chris Martin <ch.martin@gmail.com>
2017-02-23 18:14:27 -0500

    update watch script

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 find . -not -path '*/\.*' -not -path '*/_*' -not -path '*/ideaHaskellLib/*' |
-    entr -c -d bash -c 'stack exec site build'
+    entr -d bash -c 'stack exec -- site watch'
