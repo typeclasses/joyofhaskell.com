1ebb0d4c04bd7874cd8a014747003f0a24e3ee77
GinBaby <jdog74@gmail.com>
2017-02-23 21:05:06 -0600

    bootstrap: the bad beginnings

--- contact.markdown
+++ contact.markdown
@@ -2,10 +2,4 @@
 title: Contact
 ---
 
-Please send questions and feedback to
-[hello@joyofhaskell.com](mailto:hello@joyofhaskell.com).
 
-Chat with us on Twitter:
-[\@joyofhaskell](https://twitter.com/joyofhaskell),
-[\@argumatronic](https://twitter.com/argumatronic),
-[\@chris__martin](https://twitter.com/chris__martin).
--- site.hs
+++ site.hs
@@ -1,67 +1,124 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Stephen Diehl 2013
+-- License   :  MIT
+-- Maintainer:  stephen.m.diehl@gmail.com
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
 
+module Main where
 
---------------------------------------------------------------------------------
-main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+import Hakyll
+import Text.Pandoc
+import Data.Monoid ((<>))
+import qualified Data.Map as M
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+--------------------------------------------------------------------
+-- Contexts
+--------------------------------------------------------------------
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateBodyCompiler
-
-
---------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y"
+  <> defaultContext
+
+-- mathCtx :: Context String
+-- mathCtx = field "mathjax" $ \item -> do
+--   metadata <- getMetadata $ itemIdentifier item
+--   return $ if "mathjax" `M.member` metadata
+--            then "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>"
+--            else ""
+
+archiveCtx posts =
+  listField "posts" postCtx (return posts)
+  <> constField "title" "Archives"
+  <> defaultContext
+
+indexCtx posts =
+  listField "posts" postCtx (return posts)
+  <> constField "title" "Home"
+  <> defaultContext
+
+--------------------------------------------------------------------
+-- Rules
+--------------------------------------------------------------------
+
+static :: Rules ()
+static = do
+  match "fonts/*" $ do
+    route idRoute
+    compile $ copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile $ copyFileCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+  -- match "js/*" $ do
+  --   route idRoute
+  --   compile $ copyFileCompiler
+
+pages :: Rules ()
+pages = do
+  match "pages/*" $ do
+    route $ setExtension "html"
+    compile $ getResourceBody
+      >>= loadAndApplyTemplate "templates/default.html"    postCtx
+      >>= relativizeUrls
+
+posts :: Rules ()
+posts = do
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ compiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= relativizeUrls
+
+archive :: Rules ()
+archive = do
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" (archiveCtx posts)
+        >>= loadAndApplyTemplate "templates/default.html" (archiveCtx posts)
+        >>= relativizeUrls
+
+index :: Rules ()
+index = do
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      getResourceBody
+        >>= applyAsTemplate (indexCtx posts)
+        >>= relativizeUrls
+
+templates :: Rules ()
+templates = match "templates/*" $ compile templateCompiler
+
+--------------------------------------------------------------------
+-- Configuration
+--------------------------------------------------------------------
+
+compiler :: Compiler (Item String)
+compiler = pandocCompilerWith defaultHakyllReaderOptions pandocOptions
+
+pandocOptions :: WriterOptions
+pandocOptions = defaultHakyllWriterOptions{ writerHTMLMathMethod = MathJax "" }
+
+cfg :: Configuration
+cfg = defaultConfiguration
+
+main :: IO ()
+main = hakyllWith cfg $ do
+  static
+  pages
+  posts
+  archive
+  index
+  templates
